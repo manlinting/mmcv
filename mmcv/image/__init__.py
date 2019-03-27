95369bb74849ff0eee47bfb8bb627404587dc1d3
lincolnlin <lincolnlin@tencent.com>
2019-03-27 22:35:39 +0800

    lincoln add utils

--- mmcv/image/__init__.py
+++ mmcv/image/__init__.py
@@ -3,10 +3,11 @@ from .transforms import (bgr2gray, gray2bgr, bgr2rgb, rgb2bgr, bgr2hsv,
                          hsv2bgr, imflip, imrotate, imcrop, impad,
                          impad_to_multiple, imnormalize, imdenormalize,
                          imresize, imresize_like, imrescale)
+from .convert import tensor_to_pil, tensor_to_cv
 
 __all__ = [
     'imread', 'imwrite', 'imfrombytes', 'bgr2gray', 'gray2bgr', 'bgr2rgb',
     'rgb2bgr', 'bgr2hsv', 'hsv2bgr', 'imflip', 'imrotate', 'imcrop', 'impad',
     'impad_to_multiple', 'imnormalize', 'imdenormalize', 'imresize',
-    'imresize_like', 'imrescale'
+    'imresize_like', 'imrescale', 'tensor_to_pil', 'tensor_to_cv'
 ]
--- mmcv/utils/__init__.py
+++ mmcv/utils/__init__.py
@@ -1,17 +1,20 @@
 from .config import ConfigDict, Config
 from .misc import (is_str, iter_cast, list_cast, tuple_cast, is_seq_of,
                    is_list_of, is_tuple_of, slice_list, concat_list,
-                   check_prerequisites, requires_package, requires_executable)
+                   check_prerequisites, requires_package, requires_executable, AverageMeter)
 from .path import (is_filepath, fopen, check_file_exist, mkdir_or_exist,
-                   symlink, scandir, FileNotFoundError)
+                   symlink, scandir, clear_dir_if_exist, FileNotFoundError)
 from .progressbar import ProgressBar, track_progress, track_parallel_progress
 from .timer import Timer, TimerError, check_time
+from .eval import accuracy
+from .helpers import load_parameters, get_num_parameters, get_num_flops
 
 __all__ = [
     'ConfigDict', 'Config', 'is_str', 'iter_cast', 'list_cast', 'tuple_cast',
     'is_seq_of', 'is_list_of', 'is_tuple_of', 'slice_list', 'concat_list',
-    'check_prerequisites', 'requires_package', 'requires_executable',
+    'check_prerequisites', 'requires_package', 'requires_executable','AverageMeter',
     'is_filepath', 'fopen', 'check_file_exist', 'mkdir_or_exist', 'symlink',
-    'scandir', 'FileNotFoundError', 'ProgressBar', 'track_progress',
-    'track_parallel_progress', 'Timer', 'TimerError', 'check_time'
+    'scandir', 'clear_dir_if_exist', 'FileNotFoundError', 'ProgressBar', 'track_progress',
+    'track_parallel_progress', 'Timer', 'TimerError', 'check_time', 'accuracy', 'load_parameters',
+    'get_num_parameters', 'get_num_flops'
 ]
